8ccfef27d0857dd247ccafc3c5472a9da239223c
boozinka <69165324+boozinka@users.noreply.github.com>
2022-11-04 11:49:26 +0000

    Update f5_ltm_stats.py

--- f5_ltm_stats.py
+++ f5_ltm_stats.py
@@ -7,7 +7,8 @@
     This program expects the following API URLs as follows:
 
         https://<ip-address>/mgmt/tm/ltm/pool/members/stats
-        https://<ip-address>/mgmt/tm/ltm/virtual  
+        https://<ip-address>/mgmt/tm/ltm/virtual
+    
 """
 
 # Author: Wayne Bellward
@@ -18,10 +19,23 @@ import os
 import sys
 import pathlib
 import ipaddress
+from pprint import pprint
 from getpass import getpass
 from datetime import datetime
 from f5api_call import f5api_get_call
 
+
+def get_filename(message):
+
+    """ Get the user to input the filename they want to use to write a file """
+
+    now = datetime.now()
+    dt_str = now.strftime('%d-%m-%y_%H%M%S')
+
+    filename = input(message)
+
+    return filename, dt_str
+    
 
 def write_api(myapi, dict_type):
     
@@ -29,16 +43,16 @@ def write_api(myapi, dict_type):
         to a .csv file.
     """
 
-    now = datetime.now()
-    dt_str = now.strftime('%d-%m-%y_%H%M%S')
+    message = '\n\nPlease enter the name of the file you wish to save '
+    'without the file extension,\nactive or inactive will be '
+    'suffixed to the filename along with the date and time: '
+
+    filename, dt_str = get_filename(message)
 
     suffix = '_'+ dict_type + '_' + dt_str
 
     os.system('cls')
-    filename = input('\n\nPlease enter the name of the file you wish to save '
-                     'without the file extension,\nactive or inactive will be '
-                     'suffixed to the filename along with the date and time: '
-                     )
+
     
     filename = filename + suffix +'.csv'
 
@@ -47,6 +61,10 @@ def write_api(myapi, dict_type):
               'Virtual Server Destination Port', ',',
               'Virtual Server Description', ',',
               'Associated Pool Name', ',',
+              'Pool Member 1', ',', 'Pool Member 2', ',', 'Pool Member 3', ',',
+              'Pool Member 4', ',', 'Pool Member 5', ',', 'Pool Member 6', ',',
+              'Pool Member 7', ',', 'Pool Member 8', ',', 'Pool Member 9', ',',
+              'Pool Member 10', ',', 'Pool Member 11', ',', 'Pool Member 12',
               '\n']
 
     with open(filename, 'w') as file:
@@ -59,7 +77,8 @@ def write_api(myapi, dict_type):
             # Unpack dictionary
             virt_dest = params['virt_dest'].split('/')[2]
             virt_desc = params['virt_desc']
-            virt_pool = params['virt_pool'].split('/')[2]
+            pool_name = params['virt_pool']['pool_name'].split('/')[2]
+            pool_mems = params['virt_pool']['pool_mems']
 
             # Separate IP and port in the virtual server destination
             virt_dest_ip = virt_dest.split(':')[0]
@@ -67,10 +86,70 @@ def write_api(myapi, dict_type):
             
             # Compose line to be written
             line = [virt, ',', virt_dest_ip, ',', virt_dest_port, ',',
-                    virt_desc, ',', virt_pool, ',', '\n']
+                    virt_desc, ',', pool_name]
 
-            # Write each line to the file
+            # Unpack pool members list of dicts and add mem id to new list
+            for mem in pool_mems:
+                for mem_id in mem.keys():
+                    line.append(',')
+                    line.append(mem_id)
+
+            # Add a newline to the end of the line and write it a to the file
+            line.append('\n')
             file.writelines(line)
+                
+    print('\nThe file has been written to timestamped ', filename,
+          ' in the local directory', sep = '')
+    input('\nPress enter to return to write menu.')
+
+
+def write_poolmem_stats(virt_dict):
+    
+    """ Unpack passed dictionary and write the stats of all pool members to a
+        .csv file.
+    """
+
+    os.system('cls')
+    message = '\n\nPlease enter the name of the file you wish to save '
+    'without the file extension,\nthe the date and time will'
+    'be suffixed to the filename: '
+              
+    filename, dt_str = get_filename(message)
+    suffix = '_' + dt_str
+    
+    filename = filename + suffix +'.csv'
+
+    header = ['Pool Member Id', ',',
+              'Server Side Bits In', ',',
+              'Server Side Bits Out', ',',
+              'Server Side Current Connections', ',',
+              'Server Side Max Connections', ',',
+              'Server Side Packets In', ',',
+              'Server Side Packets Out', ',',
+              'Server Side Total Connections', ',',
+              '\n']
+
+    with open(filename, 'w') as file:
+
+        # Write header
+        file.writelines(header)
+
+        # Iterate over passed dictionary
+        for virt, params in virt_dict.items():
+            # Unpack dictionary
+            pool_mems = params['virt_pool']['pool_mems']
+
+            # Unpack pool members list of dicts and add mem id to new list
+            for mem in pool_mems:
+                for mem_id, stats in mem.items():
+                    line = [mem_id]
+                    for ss_stat, value in stats.items():
+                        line.append(',')
+                        line.append(str(value))
+
+                    # Add a newline character and write line to the file
+                    line.append('\n')
+                    file.writelines(line)
                 
     print('\nThe file has been written to timestamped ', filename,
           ' in the local directory', sep = '')
@@ -118,6 +197,7 @@ def create_virt_dict(ltm_virt):
     virt_dict = {}
     virt_list = ltm_virt['items']
 
+    # Iterate over virtual server api response and create new dict with our info
     for virt in virt_list:
         virt_name = virt['name']
         virt_pool = virt['pool']
@@ -127,8 +207,14 @@ def create_virt_dict(ltm_virt):
         except KeyError:
             virt_desc = 'No Description'
 
-        virt_dict.update({virt_name: {'virt_pool': virt_pool, 'virt_desc': virt_desc,
-                                      'virt_dest': virt_dest}})
+        virt_dict.update({virt_name: {'virt_desc': virt_desc,
+                                      'virt_dest': virt_dest,
+                                      'virt_pool': {'pool_name': virt_pool,
+                                                    'pool_mems': []
+                                                    }
+                                      }
+                          }
+                         )
 
     return virt_dict
 
@@ -147,11 +233,11 @@ def xref_pools(virt_dict, ltm_stats):
 
     # Intialise varibles
     virt_act_dict = {}
-    eval_list = []
+    #eval_list = []
 
     # X-Ref the new virt dict with LTM pools to create active and inactive dict
     for virt, values in virt_dict.items():
-        pool_ref = values['virt_pool'].replace('/', '~')
+        pool_ref = values['virt_pool']['pool_name'].replace('/', '~')
         pool_ref_prefix = 'https://localhost/mgmt/tm/ltm/pool/members/'
         pool_ref_stats = pool_ref_prefix + pool_ref + '/stats'
         pool_ref_mems = pool_ref_prefix +  pool_ref + '/members/stats'
@@ -161,15 +247,38 @@ def xref_pools(virt_dict, ltm_stats):
 
         for mem, params in ltm_mems.items():
             mem_stats = params['nestedStats']['entries']
-            ipaddr = mem_stats['addr']['description']
-            eval_list.append(mem_stats['serverside.bitsIn']['value'])
-            eval_list.append(mem_stats['serverside.bitsOut']['value'])
-            eval_list.append(mem_stats['serverside.curConns']['value'])
-            eval_list.append(mem_stats['serverside.maxConns']['value'])
-            eval_list.append(mem_stats['serverside.pktsIn']['value'])
-            eval_list.append(mem_stats['serverside.pktsOut']['value'])
-            eval_list.append(mem_stats['serverside.totConns']['value'])
 
+            # Grab all stats for that pool member
+            ss_bitsin = (mem_stats['serverside.bitsIn']['value'])
+            ss_bitsout = (mem_stats['serverside.bitsOut']['value'])
+            ss_curconns = (mem_stats['serverside.curConns']['value'])
+            ss_maxconns = (mem_stats['serverside.maxConns']['value'])
+            ss_pktsin = (mem_stats['serverside.pktsIn']['value'])
+            ss_pktsout = (mem_stats['serverside.pktsOut']['value'])
+            ss_totconns = (mem_stats['serverside.totConns']['value'])
+
+            # Grab pool member IP address and port to form member id
+            ipaddr = mem_stats['addr']['description']
+            port = mem_stats['port']['value']
+            mem_id = ipaddr + ':' + str(port)
+
+            # Create member dictionary to append to pool member list
+            mem = {mem_id: {'serverside_bitsin': ss_bitsin,
+                            'serverside_bitsout': ss_bitsout,
+                            'serverside_curconns': ss_curconns,
+                            'serverside_maxconns': ss_maxconns,
+                            'serverside_pktsin': ss_pktsin,
+                            'serverside_pktsout': ss_pktsout,
+                            'serverside_totconns': ss_totconns
+                            }
+                   }
+            virt_inact_dict[virt]['virt_pool']['pool_mems'].append(mem)
+            mem = {}
+
+            # Form list of stats to evaluate as active or inactive pool member
+            eval_list = [ss_bitsin, ss_bitsout, ss_curconns, ss_maxconns,
+                         ss_pktsin, ss_pktsout, ss_totconns,]
+            
             # If any of the stats are not 0, pop that virt into an active dict
             if any(v != 0 for v in eval_list):
                 virt_act_dict.update({virt: virt_inact_dict.pop(virt)})
@@ -190,9 +299,12 @@ def write_menu():
         print(
             """
             Write Menu
+            -----------
+            
             Q - Quit.
             1 - Write the active virtual servers to a file.
             2 - Write the inactive virtual servers to a file.
+            3 - Write all LTM pool member stats
             """
         )
 
@@ -226,18 +338,23 @@ def main():
     # Create an active & inactive dictionary of virtual srvs based on pool stats
     virt_act_dict, virt_inact_dict = xref_pools(virt_dict, ltm_stats)
 
+
     wm_val = None
     while wm_val != 'q':
         wm_val = write_menu()
         match wm_val:
             case '1':
                 os.system('cls')
-                print('\nPrint active virtual servers')
+                print('\nWriting active virtual servers to a .csv file')
                 write_api(virt_act_dict, 'active')
             case '2':
                 os.system('cls')
-                print('\nPrint inactive virtual servers')
+                print('\nWriting inactive virtual servers to a .csv file')
                 write_api(virt_inact_dict, 'inactive')
+            case '3':
+                os.system('cls')
+                print('\nWriting LTM pool member stats to a .csv')
+                write_poolmem_stats(virt_dict)                
             case 'q':
                 break
             case _:
@@ -246,7 +363,7 @@ def main():
                 input('\nPress Enter to try again.')
 
     input('\nPress enter to exit script ')
-    
+
 
 if __name__ == "__main__":
 
